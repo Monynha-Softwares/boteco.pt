0090cddfc5355d041a104c1da3d5e875f0e6dc98
[dyad] <marcelo.net77@hotmail.com>
2025-11-29 14:25:40 +0000

    [dyad] Audited codebase and removed unused files - deleted 3 file(s)

--- src/pages/Eventos.tsx
+++
@@ -1,9 +0,0 @@
-// This page was removed as part of the cleanup that deleted the "eventos" feature.
-// Kept as a harmless placeholder to avoid accidental import failures during the transition.
-import React from 'react';
-
-const EventosRemoved: React.FC = () => {
-  return null;
-};
-
-export default EventosRemoved;
